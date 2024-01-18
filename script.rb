99069ddb3a9f15492ec8f1c4b187e94725a40d1c
adrianmihail <adrianmihail00@gmail.com>
2024-01-18 18:56:41 +0200

    Upcased letters translate skip to a and r upcased

--- script.rb
+++ script.rb
@@ -2,14 +2,18 @@ def caesar_cipher (string, shift)
   letters = string.split("")
   char_letters = letters.map do |character| 
     if ("A".."Z").include?(character) || ("a".."z").include?(character)
-      character.ord + shift
+      if (91..96).include?(character.ord + shift)
+        letter_to_upcase = character.ord + shift + 6
+        letter_to_upcase.chr.upcase
+      else
+        character.ord + shift
+      end
 
     else
       character.ord
     end
   end
-  
-  return p char_letters
+
 
   cipher = char_letters.map {|char| char.chr}
   puts cipher.join()
